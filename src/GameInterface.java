903a2b81569ceae6abf36a1a07309589b2c90d53
daedraugheru <43789842+daedraugheru@users.noreply.github.com>
2018-11-30 17:37:37 -0600

    Create GameInterface.java

---
+++ src/GameInterface.java
@@ -0,0 +1,10 @@
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+
+public interface GameInterface extends Remote{
+	public void makeComment(String str);
+	public String attachToServer() throws FileNotFoundException, IOException;
+
+}
