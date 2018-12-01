34f959f486e8efb29ac8295f97f22e31cdcaa74e
daedraugheru <43789842+daedraugheru@users.noreply.github.com>
2018-12-01 00:29:09 -0600

    Update GameClient.java

--- src/GameClient.java
+++ src/GameClient.java
@@ -11,6 +11,8 @@ class threading implements Runnable
 	GameInterface stub;
 	String serverName;
 	boolean playersTurn;
+	String [][] board = new String [3][3];
+	String character;
 	
 	public threading(String host, GameInterface stub, String serverName, boolean turn)
 	{
@@ -18,18 +20,162 @@ class threading implements Runnable
 		this.stub = stub;
 		this.serverName = serverName;
 		this.playersTurn = turn;
-	}
-	public void run()
-	{
-		boolean gameOn = true;
-		
-		if(Thread.currentThread().getId() == 0)
+		if(serverName.equals("Game"))
 		{
-			
+			this.character = "X";
 		}
 		else
 		{
-			
+			this.character = "O";
+		}
+	}
+	
+	private boolean checkEndOfGame()
+	{
+		//This is for line1 horizontal all matching, if so game will end
+		
+		if(this.board[0][0].equals(this.board[0][1]) && this.board[0][0].equals(this.board[0][2]))
+		{
+			if(this.board[0][0].equals("X"))
+			{
+				System.out.println("Player1 has won!!!");
+			}
+			else
+			{
+				System.out.println("Player2 has won!!!");
+			}
+			return true;
+		}
+		
+		//This is for line2 horizontal all matching, if so game will end
+		
+		else if(this.board[1][0].equals(this.board[1][1]) && this.board[1][0].equals(this.board[1][2]))
+		{
+			if(this.board[1][0].equals("X"))
+			{
+				System.out.println("Player1 has won!!!");
+			}
+			else
+			{
+				System.out.println("Player2 has won!!!");
+			}
+			return true;
+		}
+		
+		//This is for line3 horizontal all matching, if so game will end
+		
+		else if(this.board[2][0].equals(this.board[2][1]) && this.board[2][0].equals(this.board[2][2]))
+		{
+			if(this.board[2][0].equals("X"))
+			{
+				System.out.println("Player1 has won!!!");
+			}
+			else
+			{
+				System.out.println("Player2 has won!!!");
+			}
+			return true;
+		}
+		
+		//This is for line1 vertical all matching, if so game will end
+		
+		else if(this.board[0][0].equals(this.board[1][0]) && this.board[0][0].equals(this.board[2][0]))
+		{
+			if(this.board[2][0].equals("X"))
+			{
+				System.out.println("Player1 has won!!!");
+			}
+			else
+			{
+				System.out.println("Player2 has won!!!");
+			}
+			return true;
+		}
+		
+		//This is for line2 vertical all matching, if so game will end
+		
+		else if(this.board[0][1].equals(this.board[1][1]) && this.board[0][1].equals(this.board[2][1]))
+		{
+			if(this.board[0][1].equals("X"))
+			{
+				System.out.println("Player1 has won!!!");
+			}
+			else
+			{
+				System.out.println("Player2 has won!!!");
+			}
+			return true;
+		}
+		//This is for line3 vertical all matching, if so game will end
+		
+		else if(this.board[0][2].equals(this.board[1][2]) && this.board[0][2].equals(this.board[2][2]))
+		{
+			if(this.board[0][2].equals("X"))
+			{
+				System.out.println("Player1 has won!!!");
+			}
+			else
+			{
+				System.out.println("Player2 has won!!!");
+			}
+			return true;
+		}
+		//This is for the diagonal bottom left to top right
+		
+		else if(this.board[2][0].equals(this.board[1][1]) && this.board[2][0].equals(this.board[0][2]))
+		{
+			if(this.board[0][2].equals("X"))
+			{
+				System.out.println("Player1 has won!!!");
+			}
+			else
+			{
+				System.out.println("Player2 has won!!!");
+			}
+			return true;
+		}
+		
+		//This is for the diagonal top left to bottom right
+		
+		else if(this.board[0][0].equals(this.board[1][1]) && this.board[0][0].equals(this.board[2][2]))
+		{
+			if(this.board[0][0].equals("X"))
+			{
+				System.out.println("Player1 has won!!!");
+			}
+			else
+			{
+				System.out.println("Player2 has won!!!");
+			}
+			return true;
+		}
+		
+		//If there isn't a winner decided this will happen
+		
+		else
+		{
+			System.out.println("It's your move Yugi-Boy!");
+			return false;
+		}
+	}
+	
+	public void run()
+	{
+		boolean gameOn = true;
+		/*
+		 * This if statement is the thread that will take care of the chat and inputing the players choice for the spot.
+		 */
+		if(Thread.currentThread().getId() == 0)
+		{
+			Scanner scanner = new Scanner(System.in);
+		}
+		
+		/*
+		 * This else statement is the thread that will constantly check to see if it is this players turn,
+		 */
+		
+		else
+		{
 			Registry registry = null;
 			try {
 				registry = LocateRegistry.getRegistry(host, 12250);
@@ -54,7 +200,35 @@ class threading implements Runnable
 			}
 			while(gameOn == true)
 			{
-				
+				for(int i = 0; i < 500; i++)
+				{
+					
+				}
+				if(this.playersTurn == false)
+				{
+					if(this.serverName.equals("Game"))
+					{
+						this.playersTurn = stub2.checkTurn("Game2");
+					}
+					else
+					{
+						this.playersTurn = stub2.checkTurn("Game");
+						
+					}
+					if(this.playersTurn)
+					{
+						this.board = stub2.updateBoard(this.serverName);
+						for(int i = 0; i < 3; i++)
+						{
+							System.out.println(this.board [i][0] + " | "  + this.board[i][1] + " | "  + this.board[i][2]);
+							System.out.println("_________");
+						}
+					}
+					if(this.checkEndOfGame())
+					{
+						gameOn = false;
+					}
+				}
 			}
 		}
 	}
@@ -76,9 +250,7 @@ public class GameClient {
 					position++;
 				}
 			}
-			//String givenIP = args[0];
-			//Registry registry = LocateRegistry.getRegistry(host, Integer.parseInt(givenIP));
-			Scanner scanner = new Scanner(System.in);
+			
 			Registry registry = LocateRegistry.getRegistry(host, 12250);
 			String serverName = "Game";
 			GameInterface stub = (GameInterface) registry.lookup("Game");
@@ -87,7 +259,7 @@ public class GameClient {
 				serverName = "Game2";
 				stub = (GameInterface) registry.lookup("Game2");
 			}
-			//System.out.println("Server connected");
+
 			for(int i = 0; i < 3; i++)
 			{
 				System.out.println(matrix [i][0] + " | "  + matrix[i][1] + " | "  + matrix[i][2]);
