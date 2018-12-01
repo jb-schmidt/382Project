6985cb22626d542d98b079646216e63e1851a873
daedraugheru <43789842+daedraugheru@users.noreply.github.com>
2018-12-01 00:30:00 -0600

    Update GameInterface.java

--- src/GameInterface.java
+++ src/GameInterface.java
@@ -6,5 +6,7 @@ import java.rmi.RemoteException;
 public interface GameInterface extends Remote{
 	public void makeComment(String str);
 	public String attachToServer() throws FileNotFoundException, IOException;
+	public boolean checkTurn(String serverName);
+	public String [] [] updateBoard(String serverName);
 
 }
