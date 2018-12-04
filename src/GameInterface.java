a0fe4c900d5ce2edece054587d76539da8613326
daedraugheru <43789842+daedraugheru@users.noreply.github.com>
2018-12-03 22:21:39 -0600

    Update GameInterface.java

--- src/GameInterface.java
+++ src/GameInterface.java
@@ -7,8 +7,8 @@ import java.rmi.RemoteException;
 public interface GameInterface extends Remote{
 	public void makeComment(String comment, String serverName, String host, boolean differentiatesServers)throws RemoteException, NotBoundException, IOException;
 	public String attachToServer() throws FileNotFoundException, IOException, RemoteException;
-	public boolean checkTurn(String serverName, String host, boolean differentiatesServers)throws RemoteException;
-	public String [] [] updateBoard(String serverName, String host, boolean differentiatesServers) throws RemoteException;
+	public boolean checkTurn(String serverName, String host, boolean differentiatesServers) throws RemoteException, FileNotFoundException, IOException;
+	public String [] [] updateBoard(String serverName, String host, boolean differentiatesServers) throws RemoteException, FileNotFoundException, IOException;
 	public void addToBoard(String [] [] str, String serverName, String host, boolean differentiatesServers) throws RemoteException;
 	public void passTurn(String serverName, String host, boolean differentiatesServers) throws RemoteException;
 	public GameInterface connectToOtherServer(String serverName, String host) throws RemoteException, NotBoundException;
